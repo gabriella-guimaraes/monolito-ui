32928d2ec01acaf7936aa6ebdd3a40a85d8d7d5f
gabriella-guimaraes <gabigara13@gmail.com>
2024-06-11 18:27:55 -0300

    feat: icons components

---
+++ src/Components/icons/home-icon.tsx
@@ -0,0 +1,7 @@
+export function HomeIcon() {
+    return(
+        <svg width="40" height="45" viewBox="0 0 40 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 40H12.5V25H27.5V40H35V17.5L20 6.25L5 17.5V40ZM0 45V15L20 0L40 15V45H22.5V30H17.5V45H0Z" fill="white"/>
+        </svg>
+    )
+};
